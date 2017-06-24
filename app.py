0f73566cb6b1fabc8a77efaa3dc4c8331f80b8b6
TitusEfferian <kiritosato@gmail.com>
2017-06-24 22:52:38 +0700

    first

--- app.py
+++ app.py
@@ -1,54 +1,29 @@
 from __future__ import unicode_literals
 
-
-
 import errno
-
 import os
-import re
-
 import sys
-
 import tempfile
-
 from argparse import ArgumentParser
-from urllib.request import urlopen
 
-from flask import Flask, request, abort, json
+from flask import Flask, request, abort
 
 from linebot import (
-
     LineBotApi, WebhookHandler
-
 )
-
 from linebot.exceptions import (
-
     InvalidSignatureError
-
 )
-
 from linebot.models import (
-
     MessageEvent, TextMessage, TextSendMessage,
-
     SourceUser, SourceGroup, SourceRoom,
-
     TemplateSendMessage, ConfirmTemplate, MessageTemplateAction,
-
     ButtonsTemplate, URITemplateAction, PostbackTemplateAction,
-
     CarouselTemplate, CarouselColumn, PostbackEvent,
-
     StickerMessage, StickerSendMessage, LocationMessage, LocationSendMessage,
-
     ImageMessage, VideoMessage, AudioMessage,
-
-    UnfollowEvent, FollowEvent, JoinEvent, LeaveEvent, BeaconEvent,
-
-    ImageSendMessage)
-
-
+    UnfollowEvent, FollowEvent, JoinEvent, LeaveEvent, BeaconEvent
+)
 
 app = Flask(__name__)
 
@@ -87,355 +62,190 @@ def make_static_tmp_dir():
 
 
 @app.route("/callback", methods=['POST'])
-
 def callback():
-
     # get X-Line-Signature header value
-
     signature = request.headers['X-Line-Signature']
 
-
-
     # get request body as text
-
     body = request.get_data(as_text=True)
-
     app.logger.info("Request body: " + body)
 
-
-
     # handle webhook body
-
     try:
-
         handler.handle(body, signature)
-
     except InvalidSignatureError:
-
         abort(400)
 
-
-
     return 'OK'
-
-
-
-
-def getJsonForCountry(country):
-    jsonurl = urlopen('https://restcountries.eu/rest/v2/alpha/'+country)
-    jsonpart = json.loads(jsonurl.read())
-    return jsonpart['name']
-
-
-
-def getJsonForUser(nickname,mode):
-    jsonurl = urlopen(
-        'https://osu.ppy.sh/api/get_user?k=37967304c711a663eb326dcf8b41e1a5987e2b7f&u=' + nickname + '&m=' + mode)
-    jsonpart = json.loads(jsonurl.read())
-    return jsonpart[0]['username']
-
-def getJsonForUserBest(nickname,mode,flag):
-    jsonurl=urlopen('https://osu.ppy.sh/api/get_user_best?k=37967304c711a663eb326dcf8b41e1a5987e2b7f&u='+nickname+'&m='+mode)
-    jsonpart=json.loads(jsonurl.read())
-    list = []
-    for x in range(0,5):
-        list.append(jsonpart[x])
-    return list[flag]['beatmap_id']
-def getJsonForUserBestPp(nickname,mode,flag):
-    jsonurl = urlopen('https://osu.ppy.sh/api/get_user_best?k=37967304c711a663eb326dcf8b41e1a5987e2b7f&u='+nickname+'&m='+mode)
-    jsonpart = json.loads(jsonurl.read())
-    list = []
-    for x in range(0, 5):
-        list.append(jsonpart[x])
-    return str(round(float(list[flag]['pp'])))
-def getJsonForBeatmapSetId(id,mode):
-    jsonurl=urlopen('https://osu.ppy.sh/api/get_beatmaps?k=37967304c711a663eb326dcf8b41e1a5987e2b7f&m='+mode+'&b='+id)
-    jsonpart=json.loads(jsonurl.read())
-    return jsonpart[0]['beatmapset_id']
-def getJsonForBeatmapDetails(id,mode):
-    jsonurl = urlopen('https://osu.ppy.sh/api/get_beatmaps?k=37967304c711a663eb326dcf8b41e1a5987e2b7f&m='+mode+'&b='+id)
-    jsonpart=json.loads(jsonurl.read())
-    return jsonpart[0]['title']+'-'+jsonpart[0]['version']
-
-
-def getJson(nickname, mode, token):
-    jsonurl = urlopen(
-        'https://osu.ppy.sh/api/get_user?k=37967304c711a663eb326dcf8b41e1a5987e2b7f&u=' + nickname + '&m=' + mode)
-    jsonpart = json.loads(jsonurl.read())
-
-    if len(jsonpart) == 0:
-        line_bot_api.reply_message(token,TextSendMessage(text='dont know'))
-    else:
-        if jsonpart[0]['pp_rank']==None:
-            line_bot_api.reply_message(token,TextSendMessage(text='the user has not played recently'))
-        else:
-            username = jsonpart[0]['username']
-            pp_rank = jsonpart[0]['pp_rank']
-            userid = jsonpart[0]['user_id']
-            imageurl = 'https://a.ppy.sh/' + userid
-            country_rank = jsonpart[0]['pp_country_rank']
-            country = jsonpart[0]['country']
-            carousel_template = CarouselTemplate(columns=[
-                CarouselColumn(
-                    text='global rank: ' + pp_rank + ' (#' + country_rank + ' ' + getJsonForCountry(country) + ')',
-                    thumbnail_image_url=imageurl, title=username, actions=[
-                        URITemplateAction(
-                            label='go to user', uri='https://osu.ppy.sh/u/' + username)
-                    ]),
-                CarouselColumn(
-                    text=getJsonForBeatmapDetails(getJsonForUserBest(username, mode, 0),
-                                              mode),
-                    thumbnail_image_url='https://b.ppy.sh/thumb/' + getJsonForBeatmapSetId(
-                    getJsonForUserBest(username, mode, 0), mode) + 'l.jpg', title=username + ' - ' + getJsonForUserBestPp(username, mode, 0) + 'pp', actions=[
-                        URITemplateAction(
-                            label='go to map', uri='https://osu.ppy.sh/b/' + getJsonForUserBest(username,
-                                                                                            mode,
-                                                                                            0) + '?m=' + mode)
-                    ]),
-                CarouselColumn(
-                    text=getJsonForBeatmapDetails(getJsonForUserBest(username, mode, 1),
-                                              mode),
-                    thumbnail_image_url='https://b.ppy.sh/thumb/' + getJsonForBeatmapSetId(
-                    getJsonForUserBest(username, mode, 1), mode) + 'l.jpg', title=username + ' - ' + getJsonForUserBestPp(username, mode, 1) + 'pp', actions=[
-                        URITemplateAction(
-                            label='go to map', uri='https://osu.ppy.sh/b/' + getJsonForUserBest(username,
-                                                                                            mode,
-                                                                                            1) + '?m=' + mode)
-                    ]),
-                CarouselColumn(
-                    text=getJsonForBeatmapDetails(getJsonForUserBest(username, mode, 2),
-                                              mode),
-                    thumbnail_image_url='https://b.ppy.sh/thumb/' + getJsonForBeatmapSetId(
-                    getJsonForUserBest(username, mode, 2), mode) + 'l.jpg', title=username + ' - ' + getJsonForUserBestPp(username, mode, 2) + 'pp', actions=[
-                        URITemplateAction(
-                            label='go to map', uri='https://osu.ppy.sh/b/' + getJsonForUserBest(username,
-                                                                                            mode,
-                                                                                            2) + '?m=' + mode)
-                    ]),
-                CarouselColumn(
-                    text=getJsonForBeatmapDetails(getJsonForUserBest(username, mode, 3),
-                                              mode),
-                    thumbnail_image_url='https://b.ppy.sh/thumb/' + getJsonForBeatmapSetId(
-                    getJsonForUserBest(username, mode, 3), mode) + 'l.jpg', title=username + ' - ' + getJsonForUserBestPp(username, mode, 3) + 'pp', actions=[
-                        URITemplateAction(
-                            label='go to map', uri='https://osu.ppy.sh/b/' + getJsonForUserBest(username,
-                                                                                            mode,
-                                                                                            3) + '?m=' + mode)
-                    ])
-            ])
-            template_message = TemplateSendMessage(
-                alt_text='tamachan sent a photo.', template=carousel_template)
-            line_bot_api.reply_message(token, template_message)
 
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_text_message(event):
     text = event.message.text
-    token = event.reply_token
 
-
-    if 'bot leave' not in text:
-        if text.startswith('/ctb'):
-            searchObjForCommand = re.search(r'/(.*?) ', text, re.M | re.I)
-            searchObj = re.search(r'/' + searchObjForCommand.group(1) + ' (.*?);', text+';', re.M | re.I)
-            getJson(searchObj.group(1),'2',token)
-        if text.startswith('/mania'):
-            searchObjForCommand = re.search(r'/(.*?) ', text, re.M | re.I)
-            searchObj = re.search(r'/' + searchObjForCommand.group(1) + ' (.*?);', text+';', re.M | re.I)
-            getJson(searchObj.group(1),'3',token)
-        if text.startswith('/taiko'):
-            searchObjForCommand = re.search(r'/(.*?) ', text, re.M | re.I)
-            searchObj = re.search(r'/' + searchObjForCommand.group(1) + ' (.*?);', text+';', re.M | re.I)
-            getJson(searchObj.group(1),'1',token)
-        if text.startswith('/std'):
-            searchObjForCommand = re.search(r'/(.*?) ', text, re.M | re.I)
-            searchObj = re.search(r'/' + searchObjForCommand.group(1) + ' (.*?);', text+';', re.M | re.I)
-            getJson(searchObj.group(1),'0',token)
-
-
-    else:
-        if isinstance(event.source,SourceGroup):
+    if text == 'profile':
+        if isinstance(event.source, SourceUser):
+            profile = line_bot_api.get_profile(event.source.user_id)
+            line_bot_api.reply_message(
+                event.reply_token, [
+                    TextSendMessage(
+                        text='Display name: ' + profile.display_name
+                    ),
+                    TextSendMessage(
+                        text='Status message: ' + profile.status_message
+                    )
+                ]
+            )
+        else:
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextMessage(text="Bot can't use profile API without user ID"))
+    elif text == 'bye':
+        if isinstance(event.source, SourceGroup):
+            line_bot_api.reply_message(
+                event.reply_token, TextMessage(text='Leaving group'))
             line_bot_api.leave_group(event.source.group_id)
-        elif isinstance(event.source,SourceRoom):
+        elif isinstance(event.source, SourceRoom):
+            line_bot_api.reply_message(
+                event.reply_token, TextMessage(text='Leaving group'))
             line_bot_api.leave_room(event.source.room_id)
-
+        else:
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextMessage(text="Bot can't leave from 1:1 chat"))
+    elif text == 'confirm':
+        confirm_template = ConfirmTemplate(text='Do it?', actions=[
+            MessageTemplateAction(label='Yes', text='Yes!'),
+            MessageTemplateAction(label='No', text='No!'),
+        ])
+        template_message = TemplateSendMessage(
+            alt_text='Confirm alt text', template=confirm_template)
+        line_bot_api.reply_message(event.reply_token, template_message)
+    elif text == 'buttons':
+        buttons_template = ButtonsTemplate(
+            title='My buttons sample', text='Hello, my buttons', actions=[
+                URITemplateAction(
+                    label='Go to line.me', uri='https://line.me'),
+                PostbackTemplateAction(label='ping', data='ping'),
+                PostbackTemplateAction(
+                    label='ping with text', data='ping',
+                    text='ping'),
+                MessageTemplateAction(label='Translate Rice', text='米')
+            ])
+        template_message = TemplateSendMessage(
+            alt_text='Buttons alt text', template=buttons_template)
+        line_bot_api.reply_message(event.reply_token, template_message)
+    elif text == 'carousel':
+        carousel_template = CarouselTemplate(columns=[
+            CarouselColumn(text='hoge1', title='fuga1', actions=[
+                URITemplateAction(
+                    label='Go to line.me', uri='https://line.me'),
+                PostbackTemplateAction(label='ping', data='ping')
+            ]),
+            CarouselColumn(text='hoge2', title='fuga2', actions=[
+                PostbackTemplateAction(
+                    label='ping with text', data='ping',
+                    text='ping'),
+                MessageTemplateAction(label='Translate Rice', text='米')
+            ]),
+        ])
+        template_message = TemplateSendMessage(
+            alt_text='Buttons alt text', template=carousel_template)
+        line_bot_api.reply_message(event.reply_token, template_message)
+    elif text == 'imagemap':
+        pass
+    else:
+        line_bot_api.reply_message(
+            event.reply_token, TextSendMessage(text=event.message.text))
 
 
 @handler.add(MessageEvent, message=LocationMessage)
-
 def handle_location_message(event):
-
     line_bot_api.reply_message(
-
         event.reply_token,
-
         LocationSendMessage(
-
             title=event.message.title, address=event.message.address,
-
             latitude=event.message.latitude, longitude=event.message.longitude
-
         )
-
     )
-
-
-
 
 
 @handler.add(MessageEvent, message=StickerMessage)
-
 def handle_sticker_message(event):
-
     line_bot_api.reply_message(
-
         event.reply_token,
-
         StickerSendMessage(
-
             package_id=event.message.package_id,
-
             sticker_id=event.message.sticker_id)
-
     )
 
 
-
-
-
 # Other Message Type
-
 @handler.add(MessageEvent, message=(ImageMessage, VideoMessage, AudioMessage))
-
 def handle_content_message(event):
-
     if isinstance(event.message, ImageMessage):
-
         ext = 'jpg'
-
     elif isinstance(event.message, VideoMessage):
-
         ext = 'mp4'
-
     elif isinstance(event.message, AudioMessage):
-
         ext = 'm4a'
-
     else:
-
         return
 
-
-
     message_content = line_bot_api.get_message_content(event.message.id)
-
     with tempfile.NamedTemporaryFile(dir=static_tmp_path, prefix=ext + '-', delete=False) as tf:
-
         for chunk in message_content.iter_content():
-
             tf.write(chunk)
-
         tempfile_path = tf.name
 
-
-
     dist_path = tempfile_path + '.' + ext
-
     dist_name = os.path.basename(dist_path)
-
     os.rename(tempfile_path, dist_path)
 
-
-
     line_bot_api.reply_message(
-
         event.reply_token, [
-
             TextSendMessage(text='Save content.'),
-
             TextSendMessage(text=request.host_url + os.path.join('static', 'tmp', dist_name))
-
         ])
 
 
-
-
-
 @handler.add(FollowEvent)
-
 def handle_follow(event):
-
     line_bot_api.reply_message(
-
         event.reply_token, TextSendMessage(text='Got follow event'))
 
 
-
-
-
 @handler.add(UnfollowEvent)
-
 def handle_unfollow():
-
     app.logger.info("Got Unfollow event")
 
 
-
-
-
 @handler.add(JoinEvent)
-
 def handle_join(event):
-
     line_bot_api.reply_message(
-
         event.reply_token,
-
         TextSendMessage(text='Joined this ' + event.source.type))
 
 
-
-
-
 @handler.add(LeaveEvent)
-
 def handle_leave():
-
     app.logger.info("Got leave event")
 
 
-
-
-
 @handler.add(PostbackEvent)
-
 def handle_postback(event):
-
-    if '/ping' in event.postback.data:
-
+    if event.postback.data == 'ping':
         line_bot_api.reply_message(
-
             event.reply_token, TextSendMessage(text='pong'))
 
 
-
-
-
 @handler.add(BeaconEvent)
-
 def handle_beacon(event):
-
     line_bot_api.reply_message(
-
         event.reply_token,
-
         TextSendMessage(
-
             text='Got beacon event. hwid={}, device_message(hex string)={}'.format(
-
                 event.beacon.hwid, event.beacon.dm)))
-
-
 
 
 

eb92db04a32f590d104aaf076a1d93d1d103f1cb
TitusEfferian <kiritosato@gmail.com>
2017-06-25 18:24:29 +0700

    first

--- app.py
+++ app.py
@@ -169,10 +169,7 @@ def handle_text_message(event):
 def handle_location_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        LocationSendMessage(
-            title=event.message.title, address=event.message.address,
-            latitude=event.message.latitude, longitude=event.message.longitude
-        )
+        TextSendMessage(text=event.message.longitude)
     )
 
 

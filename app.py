e88cbeffcecb65e8da775b80ae8163bc3706e10f
TitusEfferian <kiritosato@gmail.com>
2017-06-25 22:43:32 +0700

    first

--- app.py
+++ app.py
@@ -146,7 +146,7 @@ def getJsonForWeather(city,token):
         countryId = getJsonForCountry(jsonpart['city']['country'])
         carousel_template = CarouselTemplate(columns=[
             CarouselColumn(
-                text=str(int(regexMethodForHour(jsonpart['list'][4]['dt_txt']))-methodForNow()),
+                text=str(int(regexMethodForHour(jsonpart['list'][4]['dt_txt'])-methodForNow())),
                 title=jsonpart['city']['name']+', '+countryId, actions=[
                     URITemplateAction(
                         label='open in browser', uri='https://openweathermap.org/')

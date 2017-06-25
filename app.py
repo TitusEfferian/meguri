94407d5b8cfd29df17877ac411dbf626187ac4ac
TitusEfferian <kiritosato@gmail.com>
2017-06-25 21:39:32 +0700

    first

--- app.py
+++ app.py
@@ -142,7 +142,7 @@ def getJsonForWeather(city,token):
         carousel_template = CarouselTemplate(columns=[
             CarouselColumn(
                 text='test',
-                title=jsonpart['list'][0]['weather'][0]['icon'], actions=[
+                title=jsonpart['city']['name']+', '+countryId, actions=[
                     URITemplateAction(
                         label='go to user', uri='https://osu.ppy.sh')
                 ])

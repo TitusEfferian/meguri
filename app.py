457b394dd4c9e0cd831474d806fb7e7dae3f8579
TitusEfferian <kiritosato@gmail.com>
2017-06-25 18:32:23 +0700

    first

--- app.py
+++ app.py
@@ -167,9 +167,12 @@ def handle_text_message(event):
 
 @handler.add(MessageEvent, message=LocationMessage)
 def handle_location_message(event):
+    jsonurl = urlopen('http://api.openweathermap.org/data/2.5/weather?lat='+event.message.latitude+'&lon='+event.message.longitude+'&appid=fe18035f6b83c8b163d1a7a8ef934a75')
+    jsonpart = json.loads(jsonurl.read())
+    weather = jsonpart['weather']['main']
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.longitude)
+        TextSendMessage(text=weather)
     )
 
 

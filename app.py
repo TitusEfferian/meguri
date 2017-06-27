d2479c590827e1d46005dc72ec74e988ac8cc896
TitusEfferian <kiritosato@gmail.com>
2017-06-28 00:58:50 +0700

    first

--- app.py
+++ app.py
@@ -243,12 +243,12 @@ def videoMessageForSearchAPI(token,text):
     except HTTPError as err:
         if err.code == 500:
             line_bot_api.reply_message(token, TextSendMessage(text='video not supported'))
-def videoMessageForSearch(token,text):
+def videoMessageForSearch(text):
     req = Request('https://api.cognitive.microsoft.com/bing/v5.0/videos/search?q=' + text)
     req.add_header('Ocp-Apim-Subscription-Key', 'db017bc371a34c488702df1801fc8f11')
     resp = urlopen(req)
     content = json.loads(resp.read())
-    videoMessage(token,str(content['value'][0]['contentUrl']))
+    return str(content['value'][len(content['value'])]['contentUrl'])
 
 def goo_shorten_url(url):
     post_url = 'https://www.googleapis.com/urlshortener/v1/url?key=AIzaSyBDB-GF8QsWHoy7_Kc-wiTHRnrAeiJs8A8'
@@ -476,7 +476,7 @@ def handle_text_message(event):
         if text.startswith('video'):
             searchObj = re.search(r'video (.*?);', text + ';', re.M | re.I)
             replaceText = searchObj.group(1).replace(' ', '+')
-            videoMessageForSearch(token,replaceText)
+            line_bot_api.reply_message(token,TextSendMessage(text=videoMessageForSearch(replaceText)))
         if text.startswith('/image'):
             searchObj = re.search(r'/image (.*?);', text + ';', re.M | re.I)
             replaceText = searchObj.group(1).replace(' ','+')

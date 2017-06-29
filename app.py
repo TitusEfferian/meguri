8c2c470b1507b1df2dab89e15753c68d7c7d9bce
TitusEfferian <kiritosato@gmail.com>
2017-06-30 00:30:12 +0700

    first

--- app.py
+++ app.py
@@ -529,15 +529,15 @@ def handle_text_message(event):
             searchObj = re.search(r'/video (.*?);', text + ';', re.M | re.I)
             replaceText = searchObj.group(1).replace(' ', '+')
             jsonurl = urlopen(
-                'https://www.googleapis.com/youtube/v3/search?part=snippet&q='+replaceText+'&type=video&key=AIzaSyDbfeClXLMorneLuPnEILavUgZkiB-3SrM&maxResults=50')
+                'https://www.googleapis.com/youtube/v3/search?part=snippet&q='+replaceText+'&type=video&key=AIzaSyDbfeClXLMorneLuPnEILavUgZkiB-3SrM&maxResults=5')
             jsonpart = json.loads(jsonurl.read())
             used = []
-            random = randint(0,49)
+            random = randint(0,4)
             link = 'https://www.youtube.com/watch?v='+str(jsonpart['items'][random]['id']['videoId'])
             if videoMessageForSearchAPI(token,link)=='0':
                 while (videoMessageForSearchAPI(token,link)=='0'):
                     while random in used:
-                        random = randint(0, 49)
+                        random = randint(0, 4)
                     used.append(random)
                     link='https://www.youtube.com/watch?v='+str(jsonpart['items'][random]['id']['videoId'])
             else:

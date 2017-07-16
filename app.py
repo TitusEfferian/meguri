0ef05bc68a638e06bafa771753e5f99fdd693bc7
TitusEfferian <kiritosato@gmail.com>
2017-07-17 01:29:09 +0700

    first

--- app.py
+++ app.py
@@ -576,8 +576,8 @@ def handle_text_message(event):
             textanalytics(token,searchObj.group(1))
         if text.startswith('/alkitab'):
             kitab = re.search(r'/alkitab (.*?) ', text + ';', re.M | re.I)
-            pasal = re.search(r'/alkitab ' + kitab.group(1) + ' (.*?):', text + ';', re.M | re.I)
-            line_bot_api.reply_message(token,TextSendMessage(text=pasal.group(1)))
+            pasal = re.search(r'/alkitab ' + kitab.group(1) + ' (.*?):(.*)', text, re.M | re.I)
+            line_bot_api.reply_message(token,TextSendMessage(text=kitab.group(1)+' '+pasal.group(1)+':'+pasal.group(2)))
 
 
 

d716133ee0847a927e9dbc19a8a89e5607dd3b40
TitusEfferian <kiritosato@gmail.com>
2017-07-17 01:51:07 +0700

    first

--- app.py
+++ app.py
@@ -501,9 +501,9 @@ def alkitab(kitab,pasal,ayat,token):
     content = f.read().decode("utf-8")
 
     searchObj = re.search(r'<a class="nomor-ayat" href="'+url+'">\r\n\t\t\t'+ayat+'.\r\n\t\t\t</a>\r\n\t\t\t(.*?)</p>', content, re.M | re.I)
-    confirm_template = ConfirmTemplate(text='Do it?', actions=[
-        MessageTemplateAction(label='Yes', text='Yes!'),
-        MessageTemplateAction(label='No', text='No!'),
+    confirm_template = ConfirmTemplate(text=' ', actions=[
+        MessageTemplateAction(label='Next', text='Yes!'),
+        MessageTemplateAction(label='Prev', text='No!'),
     ])
     line_bot_api.reply_message(token,[TextSendMessage(text=kitab+' '+pasal+':'+ayat+'\n\n'+searchObj.group(1)),TemplateSendMessage(
         alt_text='Confirm alt text', template=confirm_template)])
